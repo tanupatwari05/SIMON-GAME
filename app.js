be1116c5b6052138e81cf72cad04bb1a6a99a1d5
tanupatwari05 <tanupatwari05@gmail.com>
2026-02-06 02:12:03 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let gameSeq = [];
+let userSeq = [];
+
+let started = false;
+let level = 0;
+
+document.addEventListener("keypress",function(){
+    if(started == false){
+        console.log("game has started");
+        started = true;
+    }
+})
